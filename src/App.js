082e1d31d7b80ab6d5bb2cf1e4c92858e500c51f
Slava Shensev <shensevwork@gmail.com>
2021-08-15 21:52:34 +0300

    add setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [count, setCount] = useState(0);
+    console.log(count)
+    console.log(setCount)
+    return (
+        <div className="App">
+          {/*  <h1>{like}</h1>*/}
+          {/*<button onClick={increment}>Increment</button>*/}
+          {/*<button onClick={()=> like -= 1}>Decrement</button>*/}
+        </div>
+    );
 }
 
 export default App;
